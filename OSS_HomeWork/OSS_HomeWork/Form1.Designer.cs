f8e958aad77de7987ba2486cf0030e65cb1da93f
DaSol Han <9511dasole@gmail.com>
2022-10-31 14:14:08 +0900

    Third

--- OSS_HomeWork/OSS_HomeWork/Form1.Designer.cs
+++ OSS_HomeWork/OSS_HomeWork/Form1.Designer.cs
@@ -47,6 +47,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button3
@@ -68,12 +70,13 @@
             this.button2.TabIndex = 47;
             this.button2.Text = "회차";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("굴림", 15F);
-            this.label10.Location = new System.Drawing.Point(650, 193);
+            this.label10.Location = new System.Drawing.Point(666, 193);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(68, 20);
             this.label10.TabIndex = 46;
@@ -83,7 +86,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("굴림", 15F);
-            this.label9.Location = new System.Drawing.Point(559, 193);
+            this.label9.Location = new System.Drawing.Point(582, 193);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(57, 20);
             this.label9.TabIndex = 45;
@@ -93,7 +96,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("굴림", 15F);
-            this.label8.Location = new System.Drawing.Point(467, 193);
+            this.label8.Location = new System.Drawing.Point(499, 193);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(57, 20);
             this.label8.TabIndex = 44;
@@ -103,7 +106,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("굴림", 15F);
-            this.label7.Location = new System.Drawing.Point(378, 193);
+            this.label7.Location = new System.Drawing.Point(411, 193);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(57, 20);
             this.label7.TabIndex = 43;
@@ -113,7 +116,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("굴림", 15F);
-            this.label5.Location = new System.Drawing.Point(283, 193);
+            this.label5.Location = new System.Drawing.Point(239, 193);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(57, 20);
             this.label5.TabIndex = 42;
@@ -130,7 +133,7 @@
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox7.Location = new System.Drawing.Point(287, 82);
+            this.textBox7.Location = new System.Drawing.Point(243, 82);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(68, 30);
             this.textBox7.TabIndex = 40;
@@ -138,7 +141,7 @@
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox5.Location = new System.Drawing.Point(382, 83);
+            this.textBox5.Location = new System.Drawing.Point(330, 82);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(68, 30);
             this.textBox5.TabIndex = 39;
@@ -146,7 +149,7 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox4.Location = new System.Drawing.Point(563, 78);
+            this.textBox4.Location = new System.Drawing.Point(503, 82);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(68, 30);
             this.textBox4.TabIndex = 38;
@@ -154,7 +157,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox3.Location = new System.Drawing.Point(471, 82);
+            this.textBox3.Location = new System.Drawing.Point(415, 82);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(68, 30);
             this.textBox3.TabIndex = 37;
@@ -162,7 +165,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox2.Location = new System.Drawing.Point(654, 82);
+            this.textBox2.Location = new System.Drawing.Point(586, 82);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(68, 30);
             this.textBox2.TabIndex = 36;
@@ -171,7 +174,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("굴림", 15F);
-            this.label4.Location = new System.Drawing.Point(194, 193);
+            this.label4.Location = new System.Drawing.Point(171, 193);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(57, 20);
             this.label4.TabIndex = 35;
@@ -181,7 +184,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("굴림", 15F);
-            this.label3.Location = new System.Drawing.Point(69, 193);
+            this.label3.Location = new System.Drawing.Point(39, 193);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(96, 20);
             this.label3.TabIndex = 34;
@@ -200,7 +203,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 15F);
-            this.label2.Location = new System.Drawing.Point(42, 86);
+            this.label2.Location = new System.Drawing.Point(12, 86);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(123, 20);
             this.label2.TabIndex = 32;
@@ -209,7 +212,7 @@
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox6.Location = new System.Drawing.Point(198, 83);
+            this.textBox6.Location = new System.Drawing.Point(160, 82);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(68, 30);
             this.textBox6.TabIndex = 31;
@@ -224,11 +227,31 @@
             this.label1.TabIndex = 30;
             this.label1.Text = "로또 번호 맞추기";
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 15F);
+            this.textBox1.Location = new System.Drawing.Point(670, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 30);
+            this.textBox1.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 15F);
+            this.label6.Location = new System.Drawing.Point(326, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 50;
+            this.label6.Text = "label6";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label10);
@@ -276,6 +299,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- OSS_HomeWork/OSS_HomeWork/Form1.cs
+++ OSS_HomeWork/OSS_HomeWork/Form1.cs
@@ -5,21 +5,45 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace OSS_HomeWork
 {
     public partial class Form1 : Form
     {
+        int count = 0;
+        List<int> list = new List<int>();
         public Form1()
         {
             InitializeComponent();
         }
+        private bool IsNullString(string str)
+        {
+            return string.IsNullOrEmpty(str); 
+        }
+        private int IsInt(object obj)
+        {
+            if(obj == null) return 0;
 
+            int check = 0;
+            bool bch = int.TryParse(obj.ToString(), out check);
+
+            if(!bch) return 0;
+
+            return 0;
+        }
+
+        private string
         private void label1_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
